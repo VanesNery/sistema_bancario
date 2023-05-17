bc939fd067363cc52804a2d0da075dd1bf3a97f8
Vanessa Nery <ssh-keygen -t ed25519-sk -C nery_van@hotmail.com>
2023-05-17 17:18:23 -0300

    inserido operacao de saque

--- desafio.py
+++ desafio.py
@@ -16,6 +16,8 @@ LIMITE_SAQUES = 3
 
 while True:
     opcao = input(menu).casefold()
+    dia = datetime.now()
+    data = dia.strftime("%d") +"/"+ dia.strftime("%m") +"/"+ dia.strftime("%y")
     
     if opcao == "d":
         operacao = 'Deposito'
@@ -24,12 +26,20 @@ while True:
             print('Esse valor não é permitido, favor insera um valor positivo.')
             valor = float(input('Favor insera o valor que deseja depositar R$ '))
         saldo += valor
-        dia = datetime.now()
-        data = dia.strftime("%d") +"/"+ dia.strftime("%m") +"/"+ dia.strftime("%y")
         extrato.append(f'{data} {operacao} R${valor}')
-        print(f"Seu valor R${valor} foi depositado com sucesso")
+        print(f"Seu valor R${valor} foi realizado o {operacao} com sucesso")
     elif opcao == "s":
-        print("Saque")
+        operacao = 'Saque'
+        valor = float(input('Favor insera o valor que deseja sacar R$ '))
+        while valor > limite:
+            print(f'Valor não permitido para saque, o limite para saque nesse momento é de R$ {limite}')
+            valor = float(input('Favor insera o valor que deseja sacar R$ '))
+            if valor <= saldo:
+                saldo -= valor
+                extrato.append(f'{data} {operacao} R${valor}')
+                print(f"Seu valor R${valor} foi realizado o {operacao} com sucesso")
+            else:
+                print('Seu saldo é menor que solicitado para {operacao}')
     elif opcao == "e":
         print(extrato)
     elif opcao == "q":
